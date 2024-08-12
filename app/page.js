3425b3f9f9bd94af98fab63c09dd26958469529f
Sedanah Mashhour <99051655+Sedanah@users.noreply.github.com>
2024-08-11 20:32:56 -0400

    save

--- app/page.js
+++ app/page.js
@@ -6,12 +6,17 @@ import Box from '@mui/material/Box';
 
 export default function Home() {
   const [messages, setMessages] = useState({
-    role : 'assistance',
+    role: 'assistance',
     content: 'I am an HeadstarterAI customer support agent. How can I help you today?',
-   
+
   })
-  
+
   const [message, setMessage] = useState('')
 
-  return <Box width = "100vw" height = "100vh" display = "flex" flexDirection="column"></Box>
+  return <Box
+    width="100vw"
+    height="100vh"
+    display="flex"
+    flexDirection="column"
+    justifyContent="flex-end"></Box>
 }
